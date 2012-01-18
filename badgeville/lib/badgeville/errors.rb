eb3dcf72b3e294672ad249d4b61276b32b7a0fc2
Supraja Narasimhan <supraja.n@gmail.com>
2012-01-18 10:13:25 -0800

    Removed 'puts' statements. Set from_badgeville_json() to search for presence of root key 'errors' before it looks for its absence.

--- badgeville/lib/badgeville/errors.rb
+++ badgeville/lib/badgeville/errors.rb
@@ -5,12 +5,11 @@ module Badgeville
     # Grabs errors originating from the remote model class. The custom JSON error
     # response format may not have a root key :errors.
     #
-    # @param [ActiveSupport::JSON] json the JSON response data in custom BadgevilleJsonFormat
+    # @param [String?] json the JSON response data in custom BadgevilleJsonFormat
     # @return [Array] error messages associated with the remote model
     def from_badgeville_json(json, save_cache = false)
-      puts "Hello from_badgeville_json()"
       formatted_json_decoded = Array.new
-      json_decoded = (ActiveSupport::JSON.decode(json)) || ActiveSupport::JSON.decode(json)['errors'] rescue []
+      json_decoded = (ActiveSupport::JSON.decode(json))['errors'] || ActiveSupport::JSON.decode(json) rescue []
       json_decoded.each do |attribute_name, err_msgs|
         if err_msgs.is_a? Array
           err_msgs.each do |err_msg|
