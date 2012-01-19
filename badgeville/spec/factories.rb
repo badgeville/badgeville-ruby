e653ba7755a8194bfd8689621339bc69a82ae102
Alex Tran <alext@badgeville.com>
2012-01-19 15:07:14 -0800

    [BV-294] Updates and user tests.

--- badgeville/spec/factories.rb
+++ badgeville/spec/factories.rb
@@ -1,10 +1,15 @@
 FactoryGirl.define do
+  
+  factory :user do
+    name "visitor_username"
+    network_id "4d5dc61ed0c0b32b79000001"
+    email "visitor@emailserver.com"
+    password "testing123"
+  end
   factory :player do
     email 'supraja220494@badgeville.com'
     id '4f0b29bca76865329a0000ae'
   end
-end
-FactoryGirl.define do
   factory :site do
     name "My Website"
     url "mydomain.com"
