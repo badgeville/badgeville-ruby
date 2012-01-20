490d92132408ad48ae495783ccd3fdf21725c909
Supraja Narasimhan <supraja.n@gmail.com>
2012-01-20 15:56:05 -0800

    Removed debugging statements from from_badgeville_json().

--- lib/badgeville/errors.rb
+++ lib/badgeville/errors.rb
@@ -8,27 +8,18 @@ module Badgeville
     # @param [String?] json the JSON response data in custom BadgevilleJsonFormat
     # @return [Array] error messages associated with the remote model
     def from_badgeville_json(json, save_cache = false)
-      p "HELLO"
       formatted_json_decoded = Array.new
-      #debugger
       json_decoded = (ActiveSupport::JSON.decode(json))['errors'] || ActiveSupport::JSON.decode(json) rescue []
-       p "This is json_decoded: #{json_decoded}"
       json_decoded.each do |attribute_name, err_msgs|
         if err_msgs.is_a? Array
-          p "IS ARRAY"
           err_msgs.each do |err_msg|
-            p attribute_name, "IS ATTRIBUTE NAME"
             formatted_json_decoded.push(attribute_name.humanize + " #{err_msg}")
           end
         elsif err_msgs.is_a? String
-          p "IS STRING"
           formatted_json_decoded.push(attribute_name, err_msgs)
-        else
-          p "IS NEITHER"
         end
       end
       from_array formatted_json_decoded, save_cache
     end
-
   end
 end
