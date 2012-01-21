47c612f375c5eaf3028c1990077be95f7ec40c5c
Alex Tran <alext@badgeville.com>
2012-01-20 16:19:15 -0800

    [BV-294] Renamed test files to include _spec.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,18 +1,18 @@
 FactoryGirl.define do
-  factory :user do
-    name "visitor_username"
-    network_id "4d5dc61ed0c0b32b79000001"
-    email "visitor@emailserver.com"
-    password "testing123"
-  end
+  # factory :user do
+  #   name "visitor_username"
+  #   network_id "4d5dc61ed0c0b32b79000001"
+  #   email "visitor@emailserver.com"
+  #   password "testing123"
+  # end
   factory :player do
     email 'supraja220494@badgeville.com'
     id '4f0b29bca76865329a0000ae'
   end
-  factory :site do
-    name "My Website"
-    url "mydomain.com"
-    network_id '4d5dc61ed0c0b32b79000001'
-  end
+  # factory :site do
+  #   name "My Website"
+  #   url "mydomain.com"
+  #   network_id '4d5dc61ed0c0b32b79000001'
+  # end
 end
 
