95f0b223dd3fc1be07152cb089929fee6ff30b76
Supraja Narasimhan <supraja.n@gmail.com>
2012-01-25 09:32:29 -0800

    Simplified tests for base_resource_spec.rb

---
+++ spec/readme_usecase_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+rand1 = rand(5000)
+rand2 = rand(5000)
--- spec/unit/base_resource_spec.rb
+++ spec/unit/base_resource_spec.rb
@@ -2,29 +2,24 @@ require "spec_helper"
 
 module BadgevilleBerlin
 
-  describe BaseResource, "method errors()" do
+  describe BaseResource, ".errors" do
+    before do
+      @mock_user_without_err_obj = User.new
+      @mock_user_with_errors_obj = User.new
+      @mock_user_with_errors_obj.errors.add(:base, "Mock error string.")
+    end
 
-    context "the BaseResource (User) already has an associated BadgevilleBerlin::Errors object" do
-      before do
-        @mock_user_without_err_obj = User.new
-      end
-      it "should call the new method for BadgevilleBerlin::Errors, rather than ActiveResource::Errors" do
+    context "BaseResource has associated BadgevilleBerlin::Errors object" do
+      it "should call .new from BadgevilleBerlin::Errors (not from ActiveResource::Errors)" do
         BadgevilleBerlin::Errors.should_receive( :new )
         @mock_user_without_err_obj.errors
       end
     end
 
-    context "the BaseResource (User) does not yet have an associated BadgevilleBerlin::Errors object" do
-      before do
-        @mock_user_with_errors_object = User.new
-        @mock_user_with_errors_object.errors.add(:base, "Here is a mock error message string.")
-      end
-      it "should not call the new method for BadgevilleBerlin::Errors since an error is already saved" do
+    context "BaseResource does not have associated BadgevilleBerlin::Errors object" do
+      it "should not call .new for BadgevilleBerlin::Errors" do
         BadgevilleBerlin::Errors.should_not_receive( :new )
-      end
-
-      it "should have the value 'Here is a mock error message string.' saved as an array element" do
-         @mock_user_with_errors_object.errors.messages.should == {:base => ["Here is a mock error message string."]}
+        @mock_user_with_errors_obj.errors
       end
     end
 
