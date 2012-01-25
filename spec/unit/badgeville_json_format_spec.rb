ecd3508ebdbe30ae0e6b4cbc897bd587ceaec392
Supraja Narasimhan <supraja.n@gmail.com>
2012-01-25 10:36:42 -0800

    Simplified badgeville_json_format_spec.rb and errors_spec.rb.:

--- spec/unit/badgeville_json_format_spec.rb
+++ spec/unit/badgeville_json_format_spec.rb
@@ -12,51 +12,54 @@ describe BadgevilleBerlinJsonFormat, ".decode" do
   #     lambda{ @decoded_json = BadgevilleBerlinJsonFormat.decode( @nil_string ) }.should raise_error(TypeError)
   #   end
 
+
   context "with a single object" do
-    before do
-      @json_record_w_root =
-        "{\"data\":{\"name\":\"visitor_username\",\"created_at\":\"2012-01-05T10:43:42-08:00\",\"email\":\"revised_visitor@emailserver.com\",\"_id\":\"4f05ef5ea768651b3500009f\"},\"paging\":null}"
-      @json_record_without_root =
-        "{\"name\":\"visitor_username\",\"created_at\":\"2012-01-05T10:43:42-08:00\",\"email\":\"revised_visitor@emailserver.com\",\"_id\":\"4f05ef5ea768651b3500009f\"}"
-      @json_w_2_keys_data =
-        "{\"data\":{\"name\":\"visitor_username\",\"data\":\"value_of_nested_key_data\"},\"paging\":null}"
-    end
 
     it "should handle it" do
+      @json_record_w_root =
+        "{\"data\":{\"name\":\"visitor_username\",\"created_at\":\"2012-01-05T10:43:42-08:00\",\"email\":\"revised_visitor@emailserver.com\",\"_id\":\"4f05ef5ea768651b3500009f\"},\"paging\":null}"
+
       BadgevilleBerlinJsonFormat.decode(@json_record_w_root).should ==
         {"name"=>"visitor_username", "created_at"=>"2012-01-05T10:43:42-08:00", "email"=>"revised_visitor@emailserver.com", "_id"=>"4f05ef5ea768651b3500009f"}
     end
 
     it "should handle when there's a nested key 'data'" do
+      @json_w_2_keys_data =
+        "{\"data\":{\"name\":\"visitor_username\",\"data\":\"value_of_nested_key_data\"},\"paging\":null}"
+
         BadgevilleBerlinJsonFormat.decode(@json_w_2_keys_data).should == {"name"=>"visitor_username", "data" => "value_of_nested_key_data"}
     end
 
     it "should handle when there's no root key 'data'" do
+      @json_record_without_root =
+        "{\"name\":\"visitor_username\",\"created_at\":\"2012-01-05T10:43:42-08:00\",\"email\":\"revised_visitor@emailserver.com\",\"_id\":\"4f05ef5ea768651b3500009f\"}"
+
       BadgevilleBerlinJsonFormat.decode(@json_record_without_root).should ==
         {"name"=>"visitor_username", "created_at"=>"2012-01-05T10:43:42-08:00", "email"=>"revised_visitor@emailserver.com", "_id"=>"4f05ef5ea768651b3500009f"}
     end
 
   end
 
+
   context "with multiple objects" do
-    before do
+    it "should handle it" do
       @json_collection_w_root =
         '{"data":[{"name":"visitor1","_id":"4dfa6cbc888bae20b0000016"},{"name":"visitor2","_id":"4dfa8908888bae20b50000d1"}],"paging":{"current_page":1,"per_page":10}}'
-      @json_collection_without_root =
-        '[{"name":"visitor1","_id":"4dfa6cbc888bae20b0000016"},{"name":"visitor2","_id":"4dfa8908888bae20b50000d1"}]'
-    end
 
-    it "should handle it" do
       BadgevilleBerlinJsonFormat.decode(@json_collection_w_root).should ==
         [ {"name" => "visitor1", "_id" => "4dfa6cbc888bae20b0000016"},
           {"name" => "visitor2", "_id" => "4dfa8908888bae20b50000d1"} ]
     end
 
     it "should handle when there's no root key 'data'" do
+      @json_collection_without_root =
+        '[{"name":"visitor1","_id":"4dfa6cbc888bae20b0000016"},{"name":"visitor2","_id":"4dfa8908888bae20b50000d1"}]'
+
       BadgevilleBerlinJsonFormat.decode(@json_collection_without_root).should ==
         [ {"name" => "visitor1", "_id" => "4dfa6cbc888bae20b0000016"},
           {"name" => "visitor2", "_id" => "4dfa8908888bae20b50000d1"} ]
     end
   end
+
 end
 
--- spec/unit/errors_spec.rb
+++ spec/unit/errors_spec.rb
@@ -3,89 +3,43 @@ require "spec_helper"
 module BadgevilleBerlin
   describe Errors, ".from_badgeville_berlin_json" do
     before do
-
-      # Create a player and a Errors object for that player
-      @mock_user = User.new
-      @mock_error  = Errors.new(@mock_user)
-
-      # Stub the new method to return the "mock" Errors object
+      @mock_user  = User.new
+      @mock_error = Errors.new(@mock_user)
       Errors.stub(:new).and_return(@mock_error);
     end
 
 
-    context "where JSON string is missing root key 'errors'" do
-      before do
-        json_no_root_key = "{\"email\":[\"user email is already taken\"]}"
-        @mock_error.from_badgeville_berlin_json( json_no_root_key )
-      end
-
-      it "should return a mock_error object with error messages appended" do
-        @mock_error.messages.should == {:base=>["Email user email is already taken"]}
-      end
-
-      it "should return a mock_error object with error messages appended" do
-        @mock_user.errors.messages.should == {:base=>["Email user email is already taken"]}
-      end
+    it "should handle nils" do
+       @mock_error.from_badgeville_berlin_json( nil )
+       @mock_error.messages.should == {}
     end
 
 
-    context "where JSON string has a root key 'errors'" do
-      before do
+    context "without root key 'errors'" do
+
+      it "should set error messages for errors object" do
+        json_without_root_key = "{\"email\":[\"user email is already taken\"]}"
+        @mock_error.from_badgeville_berlin_json( json_without_root_key )
+        @mock_error.messages.should == {:base=>["Email user email is already taken"]}
+      end
+
+    end
+
+
+    context "with root key 'errors'" do
+
+      it "should set error messages for errors object" do
         json_w_root_key = "{\"errors\": {\"email\":[\"user email is already taken\"]}}"
         @mock_error.from_badgeville_berlin_json( json_w_root_key )
-      end
-
-      it "should return a mock_error object with error messages appended" do
         @mock_error.messages.should == {:base=>["Email user email is already taken"]}
       end
 
-      it "should return a mock_error object with error messages appended" do
-        @mock_user.errors.messages.should == {:base=>["Email user email is already taken"]}
-      end
-    end
-
-    #   #(ActiveSupport::JSON.decode(json))['errors']
-    #   #MultiJson::DecodeError Exception: 710: unexpected token at '{"errors": {"errors":["user email is already taken"]}'
-    context "where  JSON string has root key 'errors', and a nested key 'errors'" do
-      before do
-        @json_two_errors_keys = "{\"errors\": {\"errors\":[\"user email is already taken\"]}"
-        @mock_error.from_badgeville_berlin_json( @json_two_errors_keys )
-      end
-
-      it "should return a mock_error object with no error messages" do
-          @mock_error.messages.should == {}
-      end
-
-      it "should return a mock_error object with no error messages" do
-          @mock_user.errors.messages.should == {}
-      end
-    end
-
-
-    context "where JSON string has root key 'errors', but the value at the key is an empty hash" do
-      before do
-        json_root_val_empty = "{\"errors\": {}"
-        @mock_error.from_badgeville_berlin_json( json_root_val_empty )
-      end
-
-      it "should return a mock_error object with no error messages" do
-        @mock_error.messages.should == {}
-      end
-
-      it "should return a mock_error object with no error messages" do
-        @mock_user.errors.messages.should == {}
-      end
-    end
-
-
-    context "where the JSON string is an empty" do
-      before do
-        @empty_string = ''
-        @mock_error.from_badgeville_berlin_json( @empty_string )
-      end
-
-      it "should return a mock_error object with no error messages" do
-        @mock_error.messages.should == {}
+      #   #(ActiveSupport::JSON.decode(json))['errors']
+      #   #MultiJson::DecodeError Exception: 710: unexpected token at '{"errors": {"errors":["user email is already taken"]}'
+      it "error messages should be set to empty when there is a nested key 'errors'" do
+         @json_two_errors_keys = "{\"errors\": {\"errors\":[\"user email is already taken\"]}"
+         @mock_error.from_badgeville_berlin_json( @json_two_errors_keys )
+         @mock_error.messages.should == {}
       end
     end
 
